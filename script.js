16c920434840fb60a96a0daba67420ea07f4298c
Dharmendra Mahilang <161195208+Dharmendra110@users.noreply.github.com>
2026-01-05 22:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    // take a input from radius
+    let radius = document.getElementById("radius").value;
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    // 2. convert to number
+    radius = parseFloat(radius);
+
+    // 3. Validation: if radius negative or positive
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById("volume").value = NaN;
+    } else {
+        // 4. gettinh volume
+        let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+        // 5. take Result to 4 decimal plac
+        document.getElementById("volume").value = volume.toFixed(4);
+    }
+
+    // stop to form for default sumbit
+    return false;
+}
+
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
